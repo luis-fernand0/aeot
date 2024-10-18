abcd90c16266b68455c511eb3ce9176645d07457
luis-fernando-job <fernandoluis270@gmail.com>
2024-10-18 17:00:04 -0300

    page-login-finalizada

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import '../style/login_page/login.css'
 
 const Login = () => {
   return (
     <>
-        <div className="container-login">
-    </div>
+      <div className="container-login">
+
+        <form className='form-login' action="">
+
+          <div className="logo-inputs">
+
+            <div className='logo-title'>
+              <img src="../../public/placeholder_logo.png" alt="logo-aeot" />
+              <h1 className='title-login'>Entre</h1>
+            </div>
+
+            <div className="inputs-btns">
+              <input className='input-login' type="email" name="email" id="" placeholder="Email" required autoComplete='off' />
+
+              <input className='input-login' type="password" name="passaword" id="" placeholder="Senha" required/>
+
+              <button className='btn-log btn-create'>Crie sua conta</button>
+              <button className='btn-log btn-esqueceu'>Esqueceu a senha?</button>
+            </div>
+
+          </div>
+
+          <button className='btn-log btn-login' type="submit">Login</button>
+
+        </form >
+
+      </div >
     </>
   )
 }
