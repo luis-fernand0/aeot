cb75bf04ad1c6f6998876cc31213e17fa97599b9
Luis Fernando <fernandoluis270@gmail.com>
2024-10-21 07:36:15 -0400

    add-nova-rota

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import App from './App.jsx'
 import './index.css'
 
+// CONFIGURANDO ROTAS DO REACT ROUTER
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Cadastro from './pages/cadastro.jsx'
+import Login from './pages/login.jsx'
+import Home from './pages/home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+
+  {
+    path: '/cadastro',
+    element: <Cadastro />
+  },
+
+  {
+    path: '/home',
+    element: <Home />
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
