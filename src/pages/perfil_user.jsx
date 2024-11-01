222c160e44a3087b59de107e26edf00342b36c92
Luis Fernando <fernandoluis270@gmail.com>
2024-10-31 21:15:20 -0400

    importando-component-header

--- src/pages/perfil_user.jsx
+++ src/pages/perfil_user.jsx
@@ -1,12 +1,30 @@
 import Header from "../components/header"
 
 const PerfilUser = () => {
-  return (
-    <>
-        <Header />
-        <h1>perfillllllllllllllllllllll</h1>
-    </>
-  )
+    return (
+        <>
+            <div className="container-perfil">
+                <Header />
+
+                <div className="container-logo-title">
+                    <div>
+                        <img className="logo-aeot-perfil" src="/logo_AEOT.png" alt="aeot-logo" />
+                    </div>
+                    <p>Aplicativo Enche o Tanque</p>
+                </div>
+
+                <div className="foto-user-datas">
+                    <img src="" alt="" />
+
+                    <div className="data-user-perfil">
+                        <p>Nome:</p>
+                        <p>Veiculo:</p>
+                        <p>Placa:</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default PerfilUser
