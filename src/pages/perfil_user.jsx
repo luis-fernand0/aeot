7329f539b27ff917a536b648027d43f33869b6c2
Luis Fernando <fernandoluis270@gmail.com>
2024-11-04 07:44:25 -0400

    estilizando-pagina-perfil

--- src/pages/perfil_user.jsx
+++ src/pages/perfil_user.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/header"
+import '../style/perfil_user_page/perfil_user.css'
 
 const PerfilUser = () => {
     return (
@@ -7,14 +8,14 @@ const PerfilUser = () => {
                 <Header />
 
                 <div className="container-logo-title">
-                    <div>
+                    <div className="container-logo">
                         <img className="logo-aeot-perfil" src="/logo_AEOT.png" alt="aeot-logo" />
                     </div>
-                    <p>Aplicativo Enche o Tanque</p>
+                    {/* <p>Aplicativo Enche o Tanque</p> */}
                 </div>
 
                 <div className="foto-user-datas">
-                    <img src="" alt="" />
+                    <img src="https://placehold.co/250x400" alt="" />
 
                     <div className="data-user-perfil">
                         <p>Nome:</p>
