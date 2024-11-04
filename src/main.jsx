6d62cb3af8e5d2dbbc07d1d82263e2f38a1497c0
luis-fernando-job <fernandoluis270@gmail.com>
2024-11-04 16:52:46 -0400

    adicionei-e-troquei-nomes-da-rota

--- src/main.jsx
+++ src/main.jsx
@@ -24,12 +24,12 @@ const router = createBrowserRouter([
   },
 
   {
-    path: '/home/:id',
+    path: '/home/:cod_driver',
     element: <Home/>
   },
 
   {
-    path: '/perfil',
+    path: '/perfil/:cod_driver',
     element: <PerfilUser/>
   }
 ])
