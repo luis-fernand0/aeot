a48acf5a75a0656ca6d8e3bcb9952b136948f466
luis-fernando-job <fernandoluis270@gmail.com>
2024-11-05 11:10:21 -0400

    adicionei-nova-classe

--- src/pages/perfil_user.jsx
+++ src/pages/perfil_user.jsx
@@ -10,13 +10,13 @@ const PerfilUser = () => {
     const [dataUser, setDataUser] = useState()
     const { cod_driver } = useParams()
 
-    async function callInfoUser () {
+    async function callInfoUser() {
         const response = await fetch(`${urlData}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ "cod_driver": `${cod_driver}`})
+            body: JSON.stringify({ "cod_driver": `${cod_driver}` })
         })
         const data = await response.json()
 
@@ -38,19 +38,22 @@ const PerfilUser = () => {
                                 <img className="logo-aeot-perfil" src="/logo_AEOT.png" alt="aeot-logo" />
                             </div>
                             {/* <p>Aplicativo Enche o Tanque</p> */}
-                        </div><div className="foto-user-datas">
-                            <img className="foto-user" src="https://placehold.co/250x400" alt="" />
+                        </div>
+                        <div className="foto-user-datas">
+                            <div className="foto-user-container">
+                                <img className="foto-user" src="https://placehold.co/200x200" alt="" />
+                            </div>
 
                             <div className="data-user-perfil">
-                                <p className="data-user">Nome: 
+                                <p className="data-user">Nome:
                                     <span className="data-user-span"> {dataUser.full_name}</span>
                                 </p>
 
-                                <p className="data-user">Veiculo: 
+                                <p className="data-user">Veiculo:
                                     <span className="data-user-span"> {dataUser.car_model}</span>
                                 </p>
 
-                                <p className="data-user">Placa: 
+                                <p className="data-user">Placa:
                                     <span className="data-user-span"> {dataUser.car_plate}</span>
                                 </p>
                             </div>
