a2e57707ad2055d2e034324dfdfaf4887df5f198
Luis Fernando <fernandoluis270@gmail.com>
2024-11-07 21:35:29 -0400

    adicionei-model-para-redirecionamento

--- src/pages/esqueceu_senha.jsx
+++ src/pages/esqueceu_senha.jsx
@@ -44,6 +44,10 @@ const EsqueceuSenha = () => {
         }
     }
 
+    function closeWindowResponse(btnClose) {
+        document.querySelector(`.${btnClose}`).classList.add('container-response-esqueceu-senha-hidden')
+    }
+
     async function submitForm(event) {
         event.preventDefault()
 
@@ -55,77 +59,106 @@ const EsqueceuSenha = () => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(data)
+            },
+            body: JSON.stringify(data)
         })
+        const dataResponse = await response.json()
+        if (response.status != 200 || response.status === 200) {
+            const element = document.querySelector('.container-response-esqueceu-senha')
+            element.classList.remove('container-response-esqueceu-senha-hidden')
+
+            const textResponse = document.querySelector('.response-text-esqueceu-senha')
+            console.log(dataResponse.message)
+            textResponse.innerHTML = dataResponse.message
+        }
     }
 
     return (
         <>
-            <form id='myFormEsqueceuSenha' className='form-esqueceu-senha' onSubmit={(event) => { submitForm(event) }}>
-                <div className='div-email-pass-esqueceu-senha'>
-                    <Link to={'/'}>
-                        <FontAwesomeIcon className='arrow-esqueceu-senha' icon={faArrowLeftLong} />
-                    </Link>
+            <div className='container-esqueceu-senha'>
+                <div className='container-response-esqueceu-senha container-response-esqueceu-senha-hidden'>
+                    <div className='container-info-response-esqueceu-senha'>
+                        <div className='div-close-alert-response-esqueceu-senha'>
+                            <button onClick={() => { closeWindowResponse('container-response-esqueceu-senha') }} className='btn-close-alert-response-esqueceu-senha'>
+                                X
+                            </button>
+                        </div>
+                        <div className='container-text-esqueceu-senha'>
+                            <p className='response-text-esqueceu-senha'></p>
 
-                    <input
-                        type="email"
-                        name="email_esqueceu_senha"
-                        id="email-esqueceu-senha"
-                        className='input-esqueceu-senha input-esqueceu-senha-email'
-                        placeholder='Email'
-                        required />
-
-                    <div className='div-pass-esqueceu-senha'>
-                        <input
-                            type={viewPass['senha-atual'] ? 'text' : 'password'}
-                            name="senha_atual"
-                            id="senha-atual"
-                            className='input-esqueceu-senha input-esqueceu-senha-pass'
-                            placeholder='Senha atual'
-                            minLength={6}
-                            required />
-                        <button onClick={() => { revealPass('senha-atual') }} type="button" className='pass-reveal-esqueceu-senha'>
-                            <FontAwesomeIcon className='eye-icon eye-icon-hidden' icon={viewPass['senha-atual'] ? faEye : faEyeSlash} />
-                        </button>
+                            <Link to={'/'}>
+                                <button className='btn-response-esqueceu-senha'>
+                                    Retornar para a pagina de login!
+                                </button>
+                            </Link>
+                        </div>
                     </div>
-
-                    <div className='div-pass-esqueceu-senha'>
-                        <span className='span-esqueceu-senha hidden-span-esqueceu-senha'>AS SENHAS DEVEM SER IGUAIS*</span>
-                        <input
-                            onBlur={() => { checkPass() }}
-                            type={viewPass['esqueceu-senha-pass'] ? 'text' : 'password'}
-                            name="new_senha"
-                            id="esqueceu-senha-pass"
-                            className='input-esqueceu-senha input-esqueceu-senha-pass
-                            input-esqueceu-senha-pass-new'
-                            placeholder='Nova Senha'
-                            minLength={6}
-                            required />
-                        <button onClick={() => { revealPass('esqueceu-senha-pass') }} type="button" className='pass-reveal-esqueceu-senha'>
-                            <FontAwesomeIcon className='eye-icon eye-icon-hidden' icon={viewPass['esqueceu-senha-pass'] ? faEye : faEyeSlash} />
-                        </button>
-                    </div>
-
-                    <div className='div-pass-esqueceu-senha'>
-                        <span className='span-esqueceu-senha hidden-span-esqueceu-senha'>AS SENHAS DEVEM SER IGUAIS*</span>
-                        <input
-                            onBlur={() => { checkPass() }}
-                            type={viewPass['esqueceu-senha-pass'] ? 'text' : 'password'}
-                            name="check_new_senha"
-                            id="esqueceu-senha-pass-check"
-                            className='input-esqueceu-senha input-esqueceu-senha-pass input-esqueceu-senha-pass-new'
-                            placeholder='Confirme sua Senha'
-                            minLength={6}
-                            required />
-                        <button onClick={() => { revealPass('esqueceu-senha-pass') }} type="button" className='pass-reveal-esqueceu-senha'>
-                            <FontAwesomeIcon className='eye-icon eye-icon-hidden' icon={viewPass['esqueceu-senha-pass'] ? faEye : faEyeSlash} />
-                        </button>
-                    </div>
-
-                    <button className='btn-trocar-senha' type="submit">TROCAR SENHA!</button>
                 </div>
-            </form>
+                <form id='myFormEsqueceuSenha' className='form-esqueceu-senha' onSubmit={(event) => { submitForm(event) }}>
+                    <div className='div-email-pass-esqueceu-senha'>
+                        <Link to={'/'}>
+                            <FontAwesomeIcon className='arrow-esqueceu-senha' icon={faArrowLeftLong} />
+                        </Link>
+
+                        <input
+                            type="email"
+                            name="email_esqueceu_senha"
+                            id="email-esqueceu-senha"
+                            className='input-esqueceu-senha input-esqueceu-senha-email'
+                            placeholder='Email'
+                            required />
+
+                        <div className='div-pass-esqueceu-senha'>
+                            <input
+                                type={viewPass['senha-atual'] ? 'text' : 'password'}
+                                name="senha_atual"
+                                id="senha-atual"
+                                className='input-esqueceu-senha input-esqueceu-senha-pass'
+                                placeholder='Senha atual'
+                                minLength={6}
+                                required />
+                            <button onClick={() => { revealPass('senha-atual') }} type="button" className='pass-reveal-esqueceu-senha'>
+                                <FontAwesomeIcon className='eye-icon eye-icon-hidden' icon={viewPass['senha-atual'] ? faEye : faEyeSlash} />
+                            </button>
+                        </div>
+
+                        <div className='div-pass-esqueceu-senha'>
+                            <span className='span-esqueceu-senha hidden-span-esqueceu-senha'>AS SENHAS DEVEM SER IGUAIS*</span>
+                            <input
+                                onBlur={() => { checkPass() }}
+                                type={viewPass['esqueceu-senha-pass'] ? 'text' : 'password'}
+                                name="new_senha"
+                                id="esqueceu-senha-pass"
+                                className='input-esqueceu-senha input-esqueceu-senha-pass
+                                input-esqueceu-senha-pass-new'
+                                placeholder='Nova Senha'
+                                minLength={6}
+                                required />
+                            <button onClick={() => { revealPass('esqueceu-senha-pass') }} type="button" className='pass-reveal-esqueceu-senha'>
+                                <FontAwesomeIcon className='eye-icon eye-icon-hidden' icon={viewPass['esqueceu-senha-pass'] ? faEye : faEyeSlash} />
+                            </button>
+                        </div>
+
+                        <div className='div-pass-esqueceu-senha'>
+                            <span className='span-esqueceu-senha hidden-span-esqueceu-senha'>AS SENHAS DEVEM SER IGUAIS*</span>
+                            <input
+                                onBlur={() => { checkPass() }}
+                                type={viewPass['esqueceu-senha-pass'] ? 'text' : 'password'}
+                                name="check_new_senha"
+                                id="esqueceu-senha-pass-check"
+                                className='input-esqueceu-senha input-esqueceu-senha-pass input-esqueceu-senha-pass-new'
+                                placeholder='Confirme sua Senha'
+                                minLength={6}
+                                required />
+                            <button onClick={() => { revealPass('esqueceu-senha-pass') }} type="button" className='pass-reveal-esqueceu-senha'>
+                                <FontAwesomeIcon className='eye-icon eye-icon-hidden' icon={viewPass['esqueceu-senha-pass'] ? faEye : faEyeSlash} />
+                            </button>
+                        </div>
+
+                        <button className='btn-trocar-senha' type="submit">TROCAR SENHA!</button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
