f7a682b85ee1ad715a33c3b562ee1a33a93b5cc3
Luis Fernando <fernandoluis270@gmail.com>
2024-11-21 21:45:19 -0400

    adicionando-novas-rotas

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,8 @@ import Login from './pages/login.jsx'
 import Home from './pages/home.jsx'
 import PerfilUser from './pages/perfil_user.jsx'
 import NewDrivers from './components/new_drivers.jsx'
+import AdicionarCadastros from './components/add_cadastros.jsx'
+import NewPostosServices from './components/new_postos_services.jsx'
 
 const router = createBrowserRouter([
   {
@@ -43,6 +45,16 @@ const router = createBrowserRouter([
   {
     path: '/cadastros_pendentes',
     element: <NewDrivers/>
+  },
+
+  {
+    path: '/adicionar_cadastros',
+    element: <AdicionarCadastros/>
+  },
+
+  {
+    path: '/adicionar_postos_services',
+    element: <NewPostosServices/>
   }
 ])
 
