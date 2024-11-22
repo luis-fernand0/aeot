43c31e0c3b294d3b11aa9da914416278f0f417ed
Luis Fernando <fernandoluis270@gmail.com>
2024-11-22 19:44:36 -0400

    adicionando-nova-rota-para-o-btn

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGasPump, faUser, faLock, faPlus } from '@fortawesome/free-solid-svg-icons'
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import '../style/header_component/header.css'
 
@@ -13,12 +13,12 @@ const Header = () => {
 
                 <Link to={`/perfil`}>
                     <button className='header-btn header-btn-perfil' type="button">
-                        <FontAwesomeIcon className='icon-header-btn' icon={faUser} /> 
+                        <FontAwesomeIcon className='icon-header-btn' icon={faUser} />
                         Cartão de visita
                     </button>
                 </Link>
 
-                <Link to={`/cadastros_pendentes`} className={`${typeUser === 'user' ? 'header-btn-hidden': '' }`}>
+                <Link to={`/cadastros_pendentes`} className={`${typeUser === 'user' ? 'header-btn-hidden' : ''}`}>
                     <button className='header-btn' type='button'>
                         <FontAwesomeIcon className='icon-header-btn' icon={faPlus} />
                     </button>
@@ -28,9 +28,11 @@ const Header = () => {
                     <FontAwesomeIcon className='icon-header-btn' icon={faGasPump} />
                 </button>
 
-                <button className='header-btn' type="button">
-                    <FontAwesomeIcon className='icon-header-btn' icon={faLock} />
-                </button>
+                <Link to={'/editar_perfil'}>
+                    <button className='header-btn' type="button">
+                        <FontAwesomeIcon className='icon-header-btn' icon={faLock} />
+                    </button>
+                </Link>
             </header>
         </>
     )
