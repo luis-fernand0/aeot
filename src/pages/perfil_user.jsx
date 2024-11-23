fc77e4a9e37a81494398d0438c32149e3279d2b5
Luis Fernando <fernandoluis270@gmail.com>
2024-11-23 17:24:03 -0400

    troquei-linha-de-lugar-adicionei-link-do-bucket

--- src/pages/perfil_user.jsx
+++ src/pages/perfil_user.jsx
@@ -7,13 +7,13 @@ import '../style/perfil_user_page/perfil_user.css'
 
 const urlData = import.meta.env.VITE_URL_DATAS_USER
 
-const tokenUser = localStorage.getItem('token');
-
 const PerfilUser = () => {
     const [dataUser, setDataUser] = useState()
 
     const navigate = useNavigate()
-
+    
+    const tokenUser = localStorage.getItem('token');
+    
     async function callInfoUser() {
         const response = await fetch(`${urlData}`, {
             method: 'GET',
@@ -44,11 +44,10 @@ const PerfilUser = () => {
                             <div className="container-logo">
                                 <img className="logo-aeot-perfil" src="/logo_AEOT.png" alt="aeot-logo" />
                             </div>
-                            {/* <p>Aplicativo Enche o Tanque</p> */}
                         </div>
                         <div className="foto-user-datas">
                             <div className="foto-user-container">
-                                <img className="foto-user" src={dataUser.foto} alt="" />
+                                <img className="foto-user" src={`https://aeotnew.s3.amazonaws.com/${dataUser.foto}`} alt="" />
                             </div>
 
                             <div className="data-user-perfil">
