864560675f449b6a1d7e23af932e4782480434c1
luis-fernando-job <fernandoluis270@gmail.com>
2024-11-28 17:26:03 -0400

    exportei-a-function-checkvalor

---
+++ src/functions/checkValor.js
@@ -0,0 +1,22 @@
+export function checkValor(e) {
+    var input = e.target
+    var inputValue = input.value.replace(/[^0-9]/g, '')
+
+    if (inputValue.length > 1) {
+        inputValue = inputValue.slice(0, 1) + '.' + inputValue.slice(1);
+    }
+    if (inputValue.length > 4) {
+        inputValue = input.value.replace(/[^0-9]/g, '')
+        inputValue = inputValue.slice(0, 2) + '.' + inputValue.slice(2);
+    }
+
+    if (inputValue.length > 5) {
+        inputValue = input.value.replace(/[^0-9]/g, '')
+        inputValue = inputValue.slice(0, 1) + '.' + inputValue.slice(1, 4) + '.' + inputValue.slice(4);
+    }
+    if (inputValue.length > 8) {
+      inputValue = inputValue.slice(0, 8)
+    }
+
+    input.value = inputValue
+  }
