10e8ea321116c4828022a82166a8190c4d095455
Luis Fernando <fernandoluis270@gmail.com>
2024-11-28 07:49:03 -0400

    criei-pasta-para-functions

---
+++ src/functions/validarCnpj.js
@@ -0,0 +1,17 @@
+export function validarCnpj(cnpj) {
+    const digitWeights = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+    const formattedCNPJ = String(cnpj).replace(/\W+/g, "");
+    const calculateDigit = (cnpjBase, factors) => {
+        const calculatedModule = sumDigits(cnpjBase, factors) % 11
+
+        return (calculatedModule < 2 ? 0 : 11 - calculatedModule)
+    }
+    const sumDigits = (cnpjBase, factors) => (factors.reduce((accumulator, currentValue, index) => accumulator + ((cnpjBase[index].charCodeAt(0) - 48) * currentValue), 0))
+
+    if (formattedCNPJ.length !== 14) return false;
+
+    if (formattedCNPJ[12] != calculateDigit(formattedCNPJ, digitWeights) ||
+        formattedCNPJ[13] != calculateDigit(formattedCNPJ, [6, ...digitWeights])) return false;
+
+    return true;
+}
