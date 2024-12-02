ed3cd4280abe70f3fd42f44ef65d2f22a6688bcf
luis-fernando-job <fernandoluis270@gmail.com>
2024-12-02 16:46:18 -0400

    adicionei-seta-de-navegação

--- src/components/add_cadastros.jsx
+++ src/components/add_cadastros.jsx
@@ -1,19 +1,29 @@
 import { Link } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
+
 import '../style/add_cadastros_page/add_cadastro.css'
 
 const AdicionarCadastros = () => {
   return (
     <>
-        <div className='container-btns-add-cadastro'>
-            <Link to={'/cadastros_pendentes'}>
-                <button className='btn-add-cadastro'>Cadastro Pendentes</button>
-            </Link>
-
-            <Link to={'/adicionar_postos_services'}>
-                <button className='btn-add-cadastro'>Cadastrar Posto/Serviço</button>
-            </Link>
+      <div className='container-btns-add-cadastro'>
+        <div className='container-arrow-icon'>
+          <Link to={'/home'}>
+            <FontAwesomeIcon className='arrow-icon' icon={faArrowLeftLong} />
+          </Link>
         </div>
+        <div className='container-btns-cadastro'>
+          <Link to={'/cadastros_pendentes'}>
+            <button className='btn-add-cadastro'>Cadastro Pendentes</button>
+          </Link>
+
+          <Link to={'/adicionar_postos_services'}>
+            <button className='btn-add-cadastro'>Cadastrar Posto/Serviço</button>
+          </Link>
+        </div>
+      </div>
     </>
   )
 }
