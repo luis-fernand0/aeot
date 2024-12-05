0f9fc82f6de10dc3451033ec5d723d28b320c4c6
Luis Fernando <fernandoluis270@gmail.com>
2024-12-04 23:42:47 -0400

    adicionando-btn-para-ir-para-o-maps

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -74,8 +74,7 @@ const Home = () => {
       if (local && local.latitude && local.longitude) {
         const response = await fetch(
           `${urlDataGoogleMaps}?origemlat=${local.latitude}&origemlong=${local.longitude}&address=${endereco}`
-        );
-
+        )
         if (!response.ok) {
           throw new Error('Erro ao obter os dados do Google Maps');
         }
@@ -144,185 +143,193 @@ const Home = () => {
     setEditCombustivel(false)
   }
 
-  async function exemplo() {
-    const { distancia, tempo } = await obterDistancia(posto.endereco)
-    console.log(distancia, tempo)
+  function abrirMaps(endereco) {
+    if (!local) {
+      alert('Localização atual não disponível');
+      return;
+    }
+
+    const mapsUrl = `https://www.google.com/maps/dir/?api=1&origin=${local.latitude},${local.longitude}&destination=${encodeURIComponent(endereco)}&travelmode=driving`;
+    window.open(mapsUrl, '_blank');
   }
 
-  useEffect(() => {
-    console.log('estou sendo renderizado', categoria)
-    gasStation()
-    if (postos) {
-      console.log(postos)
-    }
-  }, [categoria])
+useEffect(() => {
+  gasStation()
+}, [categoria])
 
-  useEffect(() => {
-    if (postos) {
-      postos.forEach(async (posto) => {
-        const resultado = await obterDistancia(posto.endereco, posto.cod_posto);
-        if (resultado) {
-          setDistancia((prev) => ({
-            ...prev,
-            [posto.cod_posto]: resultado, // Armazena a distância usando o cod_posto como chave
-          }));
-        }
-      });
-    }
-  }, [postos]);
+useEffect(() => {
+  if (postos) {
+    postos.forEach(async (posto) => {
+      const resultado = await obterDistancia(posto.endereco, posto.cod_posto);
+      console.log(resultado)
+      if (resultado) {
+        setDistancia((prev) => ({
+          ...prev,
+          [posto.cod_posto]: resultado,
+        }))
+      }
+    })
 
-  return (
-    <>
-      <div className="container-home">
-        <Header />
+    console.log(distancia)
+  }
+}, [postos])
 
-        <h2 className='title-home'>Todos os anuncios</h2>
+return (
+  <>
+    <div className="container-home">
+      <Header />
 
-        <div className='btns-ul'>
-          <div className='btns-fuel-services'>
-            <button onClick={() => { checkButton('postos') }} className={`btn-option btn-combustivel ${categoria === 'postos' ? 'checked' : ''}`} type="button">Postos de Combustiveis</button>
-            <button onClick={() => { checkButton('anuncios') }} className={`btn-option btn-services ${categoria === 'anuncios' ? 'checked' : ''}`} type="button">Serviços</button>
-          </div>
+      <h2 className='title-home'>Todos os anuncios</h2>
 
-          <ul className='ul-gas-services'>
-
-            {categoria === 'postos' && postos && postos.map((posto) =>
-              <li onClick={() => { callModal(posto) }} className='gas-services' key={posto.cod_posto}>
-                <img className='img-gas-services' src={`https://aeotnew.s3.amazonaws.com/${posto.foto}`} alt="imagem-do-posto-de-gasolina" />
-                <div className='info-gas-services'>
-                  <h3 className='title-gas-services'>
-                    {posto.nome}
-                  </h3>
-
-                  <p className='endereco-gas-services'>
-                    {posto.endereco}
-                  </p>
-
-                  <p className='combustiveis-gas-station'>
-                    ETANOL: R$ {posto.etanol}
-                  </p>
-                  <p className='combustiveis-gas-station'>
-                    GASOLINA: R$ {posto.gasolina}
-                  </p>
-                  <p className='combustiveis-gas-station'>
-                    DIESEL: R$ {posto.diesel}
-                  </p>
-                  {distancia[posto.cod_posto] && (
-                    <p>
-                      KM: {distancia[posto.cod_posto].distancia} Tempo estimado: {distancia[posto.cod_posto].tempo}
-                    </p>
-                  )}
-                </div>
-              </li>
-            )}
-
-            {categoria === 'anuncios' && postos && postos.map((anuncio) =>
-              <li className='gas-services' key={anuncio.cod_anuncio}>
-                <img className='img-gas-services' src={`https://aeotnew.s3.amazonaws.com/${anuncio.foto}`} alt="imagem-do-anuncio/serviço" />
-                <div className='info-gas-services'>
-                  <h3 className='title-gas-services'>
-                    {anuncio.titulo_anuncio}
-                  </h3>
-
-                  <p className='descricao-services'>
-                    {anuncio.descricao}
-                  </p>
-
-                  <p className='endereco-gas-services'>
-                    {anuncio.endereco}
-                  </p>
-                </div>
-              </li>
-            )}
-          </ul>
+      <div className='btns-ul'>
+        <div className='btns-fuel-services'>
+          <button onClick={() => { checkButton('postos') }} className={`btn-option btn-combustivel ${categoria === 'postos' ? 'checked' : ''}`} type="button">Postos de Combustiveis</button>
+          <button onClick={() => { checkButton('anuncios') }} className={`btn-option btn-services ${categoria === 'anuncios' ? 'checked' : ''}`} type="button">Serviços</button>
         </div>
 
-        {openModal && (
-          <div id='editar_posto' className='container-modal-edit-posto'>
+        <ul className='ul-gas-services'>
 
-            <div className='modal-edit-posto'>
+          {categoria === 'postos' && postos && postos.map((posto) =>
+            <li onClick={() => { callModal(posto) }} className='gas-services' key={posto.cod_posto}>
+              <img className='img-gas-services' src={`https://aeotnew.s3.amazonaws.com/${posto.foto}`} alt="imagem-do-posto-de-gasolina" />
+              <div className='info-gas-services'>
+                <h3 className='title-gas-services'>
+                  {posto.nome}
+                </h3>
 
-              <div className='container-close-modal'>
-                <button onClick={() => { closeModal('editar_posto') }} type="button" className='btn-close-modal'>
-                  <FontAwesomeIcon className='x-icon' icon={faXmark} />
-                </button>
-              </div>
+                <p className='endereco-gas-services'>
+                  {posto.endereco}
+                </p>
 
-              <div className="container-title-foto">
-                <h1 className='title-modal-posto'>{infoPostos.nome}</h1>
-                <img src={`https://aeotnew.s3.amazonaws.com/${infoPostos.foto}`} alt="foto_posto de gasolina" className='foto-modal-posto' />
-              </div>
-              <div className="container-sobre-posto">
-                <p className='info-posto posto-descricao'>{infoPostos.descricao}</p>
-                <p className='info-posto posto-endereco'>{infoPostos.endereco}</p>
-
-                <div className='container-edit-combustivel-modal'>
-                  <p id='valor-etanol' className='modal-combustivel-posto'>
-                    Etanol: R$ {infoPostos.etanol}
+                <p className='combustiveis-gas-station'>
+                  ETANOL: R$ {posto.etanol}
+                </p>
+                <p className='combustiveis-gas-station'>
+                  GASOLINA: R$ {posto.gasolina}
+                </p>
+                <p className='combustiveis-gas-station'>
+                  DIESEL: R$ {posto.diesel}
+                </p>
+                {distancia[posto.cod_posto] && (
+                  <p>
+                    KM: {distancia[posto.cod_posto].distancia} Tempo estimado: {distancia[posto.cod_posto].tempo}
                   </p>
-                  <button
-                    onClick={() => { modalEditCombustivel(infoPostos.etanol, 'etanol') }}
-                    type="button"
-                    className={`${typeUser === 'user' ? 'modal-edit-combustivel-hidden' : 'modal-edit-combustivel'}`}>
-                    <FontAwesomeIcon className='pen-icon' icon={faPen} />
-                  </button>
-                </div>
-
-                <div className='container-edit-combustivel-modal'>
-                  <p id='valor-gasolina' className='modal-combustivel-posto'>
-                    Gasolina: R$ {infoPostos.gasolina}
-                  </p>
-                  <button
-                    onClick={() => { modalEditCombustivel(infoPostos.gasolina, 'gasolina') }} type="button"
-                    className={`${typeUser === 'user' ? 'modal-edit-combustivel-hidden' : 'modal-edit-combustivel'}`}>
-                    <FontAwesomeIcon className='pen-icon' icon={faPen} />
-                  </button>
-                </div>
-
-                <div className='container-edit-combustivel-modal'>
-                  <p id='valor-diesel' className='modal-combustivel-posto'>
-                    Diesel: R$ {infoPostos.diesel}
-                  </p>
-                  <button
-                    onClick={() => { modalEditCombustivel(infoPostos.diesel, 'diesel') }} type="button"
-                    className={`${typeUser === 'user' ? 'modal-edit-combustivel-hidden' : 'modal-edit-combustivel'}`}>
-                    <FontAwesomeIcon className='pen-icon' icon={faPen} />
-                  </button>
-                </div>
+                )}
               </div>
-            </div>
-          </div>
-        )}
+            </li>
+          )}
 
-        {editCombustivel && (
-          <div id='editar_combustivel' className='container-modal-edit-combustivel'>
-            <div className='modal-edit-combustivel'>
-              <div className='container-close-modal'>
-                <button onClick={() => { closeModal('editar_combustivel') }} type="button" className='btn-close-modal'>
-                  <FontAwesomeIcon className='x-icon' icon={faXmark} />
-                </button>
+          {categoria === 'anuncios' && postos && postos.map((anuncio) =>
+            <li className='gas-services' key={anuncio.cod_anuncio}>
+              <img className='img-gas-services' src={`https://aeotnew.s3.amazonaws.com/${anuncio.foto}`} alt="imagem-do-anuncio/serviço" />
+              <div className='info-gas-services'>
+                <h3 className='title-gas-services'>
+                  {anuncio.titulo_anuncio}
+                </h3>
+
+                <p className='descricao-services'>
+                  {anuncio.descricao}
+                </p>
+
+                <p className='endereco-gas-services'>
+                  {anuncio.endereco}
+                </p>
               </div>
-              <div className='container-input-edit-combustivel'>
-                <input
-                  className='input-edit-combustivel'
-                  type="text"
-                  name={inputInfo.type_combustivel}
-                  id={inputInfo.type_combustivel}
-                  placeholder={inputInfo.valor_combustivel}
-                  onChange={(e) => { callCheckValor(e) }} />
-                <button
-                  className='btn-edit-combustivel'
-                  type="button"
-                  onClick={() => { editarCombustivel(inputInfo.type_combustivel, infoPostos.cod_posto) }}>
-                  Salvar
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
+            </li>
+          )}
+        </ul>
       </div>
-    </>
-  )
+
+      {openModal && (
+        <div id='editar_posto' className='container-modal-edit-posto'>
+
+          <div className='modal-edit-posto'>
+
+            <div className='container-close-modal'>
+              <button onClick={() => { closeModal('editar_posto') }} type="button" className='btn-close-modal'>
+                <FontAwesomeIcon className='x-icon' icon={faXmark} />
+              </button>
+            </div>
+
+            <div className="container-title-foto">
+              <h1 className='title-modal-posto'>{infoPostos.nome}</h1>
+              <img src={`https://aeotnew.s3.amazonaws.com/${infoPostos.foto}`} alt="foto_posto de gasolina" className='foto-modal-posto' />
+            </div>
+            <div className="container-sobre-posto">
+              <p className='info-posto posto-descricao'>{infoPostos.descricao}</p>
+              <p className='info-posto posto-endereco'>{infoPostos.endereco}</p>
+
+              <div className='container-edit-combustivel-modal'>
+                <p id='valor-etanol' className='modal-combustivel-posto'>
+                  Etanol: R$ {infoPostos.etanol}
+                </p>
+                <button
+                  onClick={() => { modalEditCombustivel(infoPostos.etanol, 'etanol') }}
+                  type="button"
+                  className={`${typeUser === 'user' ? 'modal-edit-combustivel-hidden' : 'modal-edit-combustivel'}`}>
+                  <FontAwesomeIcon className='pen-icon' icon={faPen} />
+                </button>
+              </div>
+
+              <div className='container-edit-combustivel-modal'>
+                <p id='valor-gasolina' className='modal-combustivel-posto'>
+                  Gasolina: R$ {infoPostos.gasolina}
+                </p>
+                <button
+                  onClick={() => { modalEditCombustivel(infoPostos.gasolina, 'gasolina') }} type="button"
+                  className={`${typeUser === 'user' ? 'modal-edit-combustivel-hidden' : 'modal-edit-combustivel'}`}>
+                  <FontAwesomeIcon className='pen-icon' icon={faPen} />
+                </button>
+              </div>
+
+              <div className='container-edit-combustivel-modal'>
+                <p id='valor-diesel' className='modal-combustivel-posto'>
+                  Diesel: R$ {infoPostos.diesel}
+                </p>
+                <button
+                  onClick={() => { modalEditCombustivel(infoPostos.diesel, 'diesel') }} type="button"
+                  className={`${typeUser === 'user' ? 'modal-edit-combustivel-hidden' : 'modal-edit-combustivel'}`}>
+                  <FontAwesomeIcon className='pen-icon' icon={faPen} />
+                </button>
+              </div>
+            </div>
+
+            <div>
+              <button onClick={() => { abrirMaps(infoPostos.endereco) }} type="button">Abrir no Maps?</button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {editCombustivel && (
+        <div id='editar_combustivel' className='container-modal-edit-combustivel'>
+          <div className='modal-edit-combustivel'>
+            <div className='container-close-modal'>
+              <button onClick={() => { closeModal('editar_combustivel') }} type="button" className='btn-close-modal'>
+                <FontAwesomeIcon className='x-icon' icon={faXmark} />
+              </button>
+            </div>
+            <div className='container-input-edit-combustivel'>
+              <input
+                className='input-edit-combustivel'
+                type="text"
+                name={inputInfo.type_combustivel}
+                id={inputInfo.type_combustivel}
+                placeholder={inputInfo.valor_combustivel}
+                onChange={(e) => { callCheckValor(e) }} />
+              <button
+                className='btn-edit-combustivel'
+                type="button"
+                onClick={() => { editarCombustivel(inputInfo.type_combustivel, infoPostos.cod_posto) }}>
+                Salvar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  </>
+)
 }
 
 export default Home
