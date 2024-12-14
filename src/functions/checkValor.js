97f78bbf8cfb89d70aaf1dcde0abadac9b3af27f
Luis Fernando <fernandoluis270@gmail.com>
2024-12-14 17:20:21 -0400

    melhorando-function-para-checar-valor

--- src/functions/checkValor.js
+++ src/functions/checkValor.js
@@ -12,8 +12,15 @@ export function checkValor(e) {
 
     if (inputValue.length > 5) {
         inputValue = input.value.replace(/[^0-9]/g, '')
+        inputValue = inputValue.slice(0, 3) + '.' + inputValue.slice(3);
+    }
+
+    if (inputValue.length > 6) {
+        inputValue = input.value.replace(/[^0-9]/g, '')
         inputValue = inputValue.slice(0, 1) + '.' + inputValue.slice(1, 4) + '.' + inputValue.slice(4);
     }
+
+
     if (inputValue.length > 8) {
       inputValue = inputValue.slice(0, 8)
     }
