57599fe831ef2cf168fe1dcd4b50578707367838
luis-fernando-job <fernandoluis270@gmail.com>
2024-12-17 17:24:00 -0400

    armazenando-dados-do-abastecimento

--- src/components/abastecimento.jsx
+++ src/components/abastecimento.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeftLong, faGasPump } from '@fortawesome/free-solid-svg-icons'
@@ -10,8 +10,46 @@ import '../style/abastecimento_component/abastecimento.css'
 const Abastecimento = () => {
     const [btnChecked, setBtnChecked] = useState('btn-litro')
 
+    const navigate = useNavigate()
+
     const callCheckValor = (e) => {
         checkValor(e)
+    }
+    function formatLitro(e) {
+        var input = e.target
+        var inputValue = input.value.replace(/[^0-9]/g, '')
+
+        if (inputValue.length > 1) {
+            inputValue = inputValue.slice(0, 1) + '.' + inputValue.slice(1);
+        }
+        if (inputValue.length > 4) {
+            inputValue = input.value.replace(/[^0-9]/g, '')
+            inputValue = inputValue.slice(0, 2) + '.' + inputValue.slice(2);
+        }
+
+        if (inputValue.length > 5) {
+            inputValue = inputValue.slice(0, 5)
+        }
+
+        input.value = inputValue
+    }
+
+    function enviarDados(combustivel, pagamento, abastecimento, valor) {
+        const Typecombustivel = document.getElementById(combustivel).value
+        const payMethod = document.getElementById(pagamento).value
+        const value = document.getElementById(valor).value
+        
+        const posto = JSON.parse(sessionStorage.getItem('detalhes'))
+
+        var metodoAbastecimento = {
+            tipo_combustivel: Typecombustivel,
+            metodo_pagamento: payMethod,
+            forma_abastecimento: abastecimento,
+            preco_ou_litro: value,
+            posto
+        }
+        navigate('/perfil')
+        sessionStorage.setItem('dadosAbastecimento', JSON.stringify(metodoAbastecimento))
     }
 
     return (
@@ -44,10 +82,10 @@ const Abastecimento = () => {
                                     Qual será a forma de pagamento?
                                 </label>
                                 <select name="pagamento" id="pagamento">
-                                    <option value="Debito">Debito</option>
-                                    <option value="Credito">Credito</option>
-                                    <option value="Dinheiro">Dinheiro</option>
-                                    <option value="Pix">Pix</option>
+                                    <option value="debito">Debito</option>
+                                    <option value="credito">Credito</option>
+                                    <option value="dinheiro">Dinheiro</option>
+                                    <option value="pix">Pix</option>
                                 </select>
                             </div>
                         </div>
@@ -92,17 +130,23 @@ const Abastecimento = () => {
                                                 Quantos litros deseja abastecer?
                                             </p>
                                             <input
+                                                onChange={(e) => { formatLitro(e) }}
+                                                id='input-litro'
                                                 className='input-option'
                                                 type="text"
                                                 placeholder='Litros' />
-                                            <Link to={'/perfil'}>
-                                                <button
-                                                    className='btn-abastecer'
-                                                    type="button">
-                                                    Abastecer!
-                                                    <FontAwesomeIcon className='gas-pump-icon' icon={faGasPump} />
-                                                </button>
-                                            </Link>
+                                            <button
+                                                onClick={() => {
+                                                    enviarDados('combustivel',
+                                                        'pagamento',
+                                                        'litro',
+                                                        'input-litro')
+                                                }}
+                                                className='btn-abastecer'
+                                                type="button">
+                                                Abastecer!
+                                                <FontAwesomeIcon className='gas-pump-icon' icon={faGasPump} />
+                                            </button>
                                         </div>
                                     </>
                                 )}
@@ -115,19 +159,22 @@ const Abastecimento = () => {
                                             </p>
                                             <input
                                                 onChange={(e) => { callCheckValor(e) }}
+                                                id='input-preco'
                                                 className='input-option'
                                                 type="text"
                                                 placeholder='Preço' />
-
-
-                                            <Link to={'/perfil'}>
-                                                <button
-                                                    className='btn-abastecer'
-                                                    type="button">
-                                                    Abastecer!
-                                                    <FontAwesomeIcon className='gas-pump-icon' icon={faGasPump} />
-                                                </button>
-                                            </Link>
+                                            <button
+                                                onClick={() => {
+                                                    enviarDados('combustivel',
+                                                        'pagamento',
+                                                        'preco',
+                                                        'input-preco')
+                                                }}
+                                                className='btn-abastecer'
+                                                type="button">
+                                                Abastecer!
+                                                <FontAwesomeIcon className='gas-pump-icon' icon={faGasPump} />
+                                            </button>
                                         </div>
                                     </>
                                 )}
@@ -138,7 +185,19 @@ const Abastecimento = () => {
                                             <p className='text-option'>
                                                 Enche o tanque!
                                             </p>
+                                            <input
+                                                onChange={(e) => { formatLitro(e) }}
+                                                id='input-tanque'
+                                                className='input-option'
+                                                type="text"
+                                                placeholder='Litros do Tanque' />
                                             <button
+                                                onClick={() => {
+                                                    enviarDados('combustivel',
+                                                        'pagamento',
+                                                        'encher-tanque',
+                                                        'input-tanque')
+                                                }}
                                                 className='btn-abastecer btn-enche-o-tanque'
                                                 type="button">
                                                 Encher o tanque!
