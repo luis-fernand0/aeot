68bed262626558f669f9a2b02c05d5ffd9961df9
Luis Fernando <fernandoluis270@gmail.com>
2024-12-26 21:03:54 -0400

    removendo-gerar-qrode

--- src/pages/perfil_user.jsx
+++ src/pages/perfil_user.jsx
@@ -1,10 +1,5 @@
 import { useState, useEffect } from "react"
 import { useNavigate } from 'react-router-dom'
-import { QRCodeCanvas } from "qrcode.react"
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
-
 
 import Header from "../components/header"
 import '../style/perfil_user_page/perfil_user.css'
@@ -12,15 +7,7 @@ import '../style/perfil_user_page/perfil_user.css'
 const urlData = import.meta.env.VITE_URL_DATAS_USER
 
 const PerfilUser = () => {
-    const abastecimento = JSON.parse(sessionStorage.getItem('dadosAbastecimento'))
-
     const [dataUser, setDataUser] = useState()
-
-    const [dataPosto, setDataPosto] = useState(abastecimento.posto || {})
-    const [dataAbastecimento, setDataAbastecimento] = useState(abastecimento || {})
-
-    const [showQRCode, setShowQRCode] = useState(false)
-    const [qrCodeValue, setQrCodeValue] = useState("")
 
     const navigate = useNavigate()
 
@@ -41,31 +28,6 @@ const PerfilUser = () => {
 
         setDataUser(data)
     }
-
-    const generateQRCode = () => {
-        const qrData = JSON.stringify({
-            usuario: dataUser.full_name,
-            veiculo: {
-                modelo: dataUser.car_model,
-                placa: dataUser.car_plate
-            },
-            posto: dataPosto[0].nome,
-            tipo_combustivel: dataAbastecimento.tipo_combustivel,
-            valor_combustivel: dataPosto[0][dataAbastecimento.tipo_combustivel],
-            metodo_pagamento: dataAbastecimento.metodo_pagamento,
-            forma_abastecimento: dataAbastecimento.forma_abastecimento,
-            quantidade: dataAbastecimento.preco_ou_litro,
-            valor_total: calcularPagamento(
-                Number(dataPosto[0][dataAbastecimento.tipo_combustivel]),
-                Number(dataAbastecimento.preco_ou_litro),
-            )
-        })
-
-        setQrCodeValue(qrData)
-        setShowQRCode(true)
-    }
-
-    const calcularPagamento = (num1, num2) => parseFloat(num1 * num2).toFixed(2)
 
     useEffect(() => {
         callInfoUser()
@@ -102,106 +64,6 @@ const PerfilUser = () => {
                             </div>
                         </div>
                     </>
-                )}
-
-                {dataAbastecimento && (
-                    <>
-                        <div className="container-dados-abastecimento">
-                            <div className="container-dados">
-
-                                <h1 className="title-dados-abastecimento">
-                                    Confirme os dados do abastecimento
-                                </h1>
-
-                                <div className="container-posto">
-                                    <h2 className="title-container title-dados-posto">
-                                        Dados do posto de combustivel
-                                    </h2>
-
-                                    <p className="text-container dado-posto-nome">
-                                        {dataPosto[0].nome}
-                                    </p>
-
-                                    <div className="container-img-posto">
-                                        <img
-                                            className="img-posto"
-                                            src={`https://aeotnew.s3.amazonaws.com/${dataPosto[0].foto}`}
-                                            alt="foto_posto" />
-                                    </div>
-
-                                    <p className="text-container dado-posto-endereco">
-                                        {dataPosto[0].endereco}
-                                    </p>
-
-                                    <p className="text-container dado-posto-combustivel">
-                                        {dataAbastecimento.tipo_combustivel}:
-                                        R$ {dataPosto[0][dataAbastecimento.tipo_combustivel]}
-                                    </p>
-                                </div>
-
-                                <div className="container-abastecimento">
-                                    <h2 className="title-container title-abastecimento">
-                                        Abastecimento
-                                    </h2>
-
-                                    <div className="container-sobre-abastecimento">
-                                        <p className="text-container abastecimento-tipo-combustivel">
-                                            Tipo de combustivel: {dataAbastecimento.tipo_combustivel}
-                                        </p>
-
-                                        <p className="text-container abastecimento-forma-abastecimento">
-                                            Vai abastecer por: {dataAbastecimento.forma_abastecimento}
-                                        </p>
-
-                                        <p className="text-container abastecimento-metodo-pagamento">
-                                            Metodo de Pagamento: {dataAbastecimento.metodo_pagamento}
-                                        </p>
-
-                                        <p className="text-container abastecimento-preco-ou-litro">
-                                            Quantidade que vai abastecer:
-                                            {dataAbastecimento.forma_abastecimento != 'preco' ?
-                                                ` ${dataAbastecimento.preco_ou_litro} Litros` :
-                                                ` R$ ${dataAbastecimento.preco_ou_litro}`}
-                                        </p>
-
-                                        <p className="text-container abastecimento-preco-ou-litro">
-                                            Valor total:
-                                            {dataAbastecimento.forma_abastecimento != 'preco' ?
-                                                ` R$ ${calcularPagamento(
-                                                    Number(dataPosto[0][dataAbastecimento.tipo_combustivel]),
-                                                    Number(dataAbastecimento.preco_ou_litro),
-                                                )}` :
-                                                `R$ ${dataAbastecimento.preco_ou_litro}`}
-                                        </p>
-                                    </div>
-                                </div>
-
-                                <button className="btn-criar-qrcode" onClick={() => { generateQRCode() }}>
-                                    Gerar QRCode de Abastecimento!
-                                </button>
-                            </div>
-                        </div>
-                    </>
-                )}
-
-                {showQRCode && (
-                    <div className="qrcode-container">
-                        <div className="qrcode-btn-close-container">
-                            <div className="container-btn-close">
-                                <button
-                                    onClick={() => { setShowQRCode(false) }}
-                                    className="btn-close">
-                                    <FontAwesomeIcon className="x-icon" icon={faXmark} />
-                                </button>
-                            </div>
-                            <div className="qrcode">
-                                <QRCodeCanvas value={qrCodeValue} size={256} />
-                                <p>
-                                    <span>*Mostre o QRCode para o frentista*</span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
                 )}
             </div>
         </>
