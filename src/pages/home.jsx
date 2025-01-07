53bca94f91a3aa1ba059fd49a2eea6858c382656
luis-fernando-job <fernandoluis270@gmail.com>
2025-01-07 16:51:29 -0400

    adicionando-regra-para-exibir-anuncios

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -143,7 +143,7 @@ const Home = () => {
       <div className="container-home">
         <Header redirectTo={'/'} />
 
-        {typerUser != 'posto' && (
+        {(typerUser === 'administrador' || typerUser === 'driver') && (
           <>
             <h2 className='title-home'>Todos os anuncios</h2>
             <div className='btns-ul'>
@@ -238,6 +238,61 @@ const Home = () => {
 
                           <p className='time km-time'>
                             <FontAwesomeIcon icon={faClock} style={{ color: "#4caf50", }} /> {distancia[anuncio.cod_anuncio].tempo}
+                          </p>
+                        </div>
+                      )}
+                    </div>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </>
+        )}
+
+        {(typerUser === 'posto' || typerUser === 'frentista') && (
+          <>
+            <h2 className='title-home'>Seu anuncio</h2>
+            <div className="btns-ul">
+              <ul className='ul-gas-services'>
+
+                {postos && postos.map((posto) =>
+                  <li onClick={() => { detalhes(posto, distancia[posto.cod_posto], local, categoria) }}
+                    className='gas-services'
+                    key={posto.cod_posto}>
+
+                    <div className='container-img-title'>
+                      <div className='container-img'>
+                        <img className='img-gas-services' src={`https://aeotnew.s3.amazonaws.com/${posto.foto}`} alt="imagem-do-posto-de-gasolina" />
+                      </div>
+
+                      <div className='container-title-endereco'>
+                        <h3 className='title-gas-services'>
+                          {posto.nome}
+                        </h3>
+
+                        <p className='endereco-gas-services'>
+                          {posto.endereco}
+                        </p>
+                      </div>
+                    </div>
+                    <div className='info-gas-services'>
+                      <p className='combustiveis-gas-station'>
+                        ETANOL: R$ {posto.etanol}
+                      </p>
+                      <p className='combustiveis-gas-station'>
+                        GASOLINA: R$ {posto.gasolina}
+                      </p>
+                      <p className='combustiveis-gas-station'>
+                        DIESEL: R$ {posto.diesel}
+                      </p>
+                      {distancia[posto.cod_posto] && (
+                        <div className='container-km-time'>
+                          <p className='km km-time'>
+                            <FontAwesomeIcon icon={faFlagCheckered} style={{ color: "#4caf50", }} /> {distancia[posto.cod_posto].distancia}
+                          </p>
+
+                          <p className='time km-time'>
+                            <FontAwesomeIcon icon={faClock} style={{ color: "#4caf50", }} /> {distancia[posto.cod_posto].tempo}
                           </p>
                         </div>
                       )}
