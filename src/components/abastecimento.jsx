59815362fccb5388bc4c1aa6e10efc36366533de
luis-fernando-job <fernandoluis270@gmail.com>
2025-02-06 16:57:00 -0400

    retirando-limitação-dos-100-litros-e-adicionando-ponto-a-partir-do-terceiro-digito

--- src/components/abastecimento.jsx
+++ src/components/abastecimento.jsx
@@ -18,16 +18,16 @@ const Abastecimento = () => {
         let input = e.target
         let inputValue = input.value.replace(/[^0-9]/g, '')
 
-        if (inputValue.length > 1) {
-            inputValue = inputValue.slice(0, 1) + '.' + inputValue.slice(1);
-        }
-        if (inputValue.length > 4) {
-            inputValue = input.value.replace(/[^0-9]/g, '')
+        if (inputValue.length > 2) {
             inputValue = inputValue.slice(0, 2) + '.' + inputValue.slice(2);
         }
-
         if (inputValue.length > 5) {
-            inputValue = inputValue.slice(0, 5)
+            inputValue = input.value.replace(/[^0-9]/g, '')
+            inputValue = inputValue.slice(0, 3) + '.' + inputValue.slice(3);
+        }
+
+        if (inputValue.length > 6) {
+            inputValue = inputValue.slice(0, 6)
         }
 
         input.value = inputValue
