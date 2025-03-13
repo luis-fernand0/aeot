1831061ff1a307c613c972df49dd9a4297ea5a44
luis-fernando-job <fernandoluis270@gmail.com>
2025-03-13 17:16:43 -0400

    criando component para adicionar brinde ao combustivel

---
+++ src/components/adicionarBrinde.jsx
@@ -0,0 +1,17 @@
+const AdicionarBrinde = () => {
+    return (
+        <>
+            <div className='container-add-brinde'>
+                <h3>
+                    Em qual combustivel deseja adicionar o brinde?
+                </h3>
+
+                <div>
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AdicionarBrinde
