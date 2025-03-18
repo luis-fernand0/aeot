4fb64de4058859c7e0dba62f929b88cd1fa78018
luis-fernando-job <fernandoluis270@gmail.com>
2025-03-18 17:04:10 -0400

    arquivo que comtem os objetos para importar e reaproveitar no codigo

---
+++ src/functions/contants.js
@@ -0,0 +1,18 @@
+export const combustiveis = [
+    { value: '1', label: 'etanol' },
+    { value: '2', label: 'gasolina' },
+    { value: '3', label: 'diesel' }
+];
+
+export const formasPagamento = [
+    { value: '1', label: 'dinheiro' },
+    { value: '2', label: 'pix' },
+    { value: '3', label: 'debito' },
+    { value: '4', label: 'credito' },
+];
+
+export const formasAbastecimentos = [
+    { value: '', label: 'Escolha a forma de abastecimento' },
+    { value: '1', label: 'Litragem Livre' },
+    { value: '2', label: 'Encher Tanque' },
+];
