08f561fa10f5f07027f5c7b497e0c304e9de4048
luis-fernando-job <fernandoluis270@gmail.com>
2025-03-27 16:52:10 -0400

    listando brindes do motorista e gerando QRCode para resgatar o brinde

--- src/pages/perfil_user.jsx
+++ src/pages/perfil_user.jsx
@@ -1,13 +1,32 @@
 import { useState, useEffect } from "react"
 import { useNavigate } from 'react-router-dom'
+import { QRCodeCanvas } from "qrcode.react"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark, faKey } from '@fortawesome/free-solid-svg-icons'
+
+import { generateKey } from "../functions/generateKey"
 
 import Header from "../components/header"
+import ListarBrindes from "../components/listarBrindes"
+import Loading from "../components/loading"
+import ModalResponse from "../components/modalResponse"
 import '../style/perfil_user_page/perfil_user.css'
 
 const urlData = import.meta.env.VITE_URL_DATAS_USER
+const urlCadastrarChave = import.meta.env.VITE_URL_CADASTRAR_CHAVE
 
 const PerfilUser = () => {
     const [dataUser, setDataUser] = useState()
+    const [exibirBrindes, setExibirBrindes] = useState(false)
+
+    const [showQrCode, setShowQRCode] = useState(false)
+    const [qrCodeValue, setQrCodeValue] = useState({ qrCode: '', chave: null })
+
+    const [loading, setLoading] = useState(false)
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [modalMessage, setModalMessage] = useState('');
+
 
     const navigate = useNavigate()
 
@@ -29,11 +48,75 @@ const PerfilUser = () => {
         setDataUser(data)
     }
 
+    async function gerarQrCode(brinde) {
+        setLoading(true)
+        console.log(brinde)
+        try {
+            let chaveID = qrCodeValue.chave || generateKey(dataUser?.placa)
+
+            const qrData = JSON.stringify({
+                tipo: 'resgate_de_brinde',
+                chave: chaveID,
+                driver_id: dataUser?.user_id,
+                usuario: dataUser?.nome,
+                foto_user: dataUser?.foto,
+                veiculo: {
+                    modelo: dataUser?.modelo,
+                    placa: dataUser?.placa
+                },
+                posto: brinde.nome_posto,
+                posto_user_id: brinde.user_id,
+                cod_brinde: brinde.cod_brinde,
+                nome_brinde: brinde.nome_brinde
+            })
+
+            if (!qrCodeValue.chave) {
+                const response = await fetch(urlCadastrarChave, {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': `Bearer ${tokenUser}`,
+                        'Content-Type': 'application/json'
+                    },
+                    body: qrData
+                })
+                const data = await response.json()
+                if (response.status === 403) {
+                    navigate('/', { replace: true })
+                    return
+                }
+
+                if (!response.ok) {
+                    setModalMessage(data.message)
+                    setModalVisible(true)
+                    return
+                }
+            }
+
+            setQrCodeValue({ qrCode: qrData, chave: chaveID })
+            setShowQRCode(true)
+
+            setTimeout(() => {
+                setQrCodeValue({ qrCode: '', chave: null })
+                setShowQRCode(false)
+            }, 300000)
+        } catch (err) {
+            setModalMessage(`Desculpe ocorreu um erro inesperado! ${err.message}`)
+            setModalVisible(true)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     useEffect(() => {
         callInfoUser()
     }, [])
     return (
         <>
+            <Loading loading={loading} />
+            <ModalResponse
+                isVisible={isModalVisible}
+                onClose={() => setModalVisible(false)}
+                message={modalMessage} />
             <div className="container-perfil">
                 <Header redirectTo={'/home'} />
 
@@ -64,7 +147,7 @@ const PerfilUser = () => {
                             </div>
 
                             <div className="container-btn-brinde">
-                                <button className="btn-brinde">
+                                <button onClick={() => { setExibirBrindes(true) }} className="btn-brinde">
                                     Meus brindes!
                                 </button>
                             </div>
@@ -72,6 +155,42 @@ const PerfilUser = () => {
                     </>
                 )}
             </div>
+
+            {exibirBrindes && (
+                <div className="container-brindes-user">
+                    <ListarBrindes
+                        clickBrinde={gerarQrCode}
+                        closeModal={setExibirBrindes}
+                        driverBrinde={true} />
+                </div>
+            )}
+
+            {showQrCode && (
+                <div className="qrcode-container">
+                    <div className="qrcode-btn-close-container">
+                        <div className="container-btn-close">
+                            <button
+                                onClick={() => { setShowQRCode(false) }}
+                                className="btn-close">
+                                <FontAwesomeIcon className="x-icon" icon={faXmark} />
+                            </button>
+                        </div>
+                        <div className="qrcode">
+                            <QRCodeCanvas value={qrCodeValue.qrCode} size={256} />
+                            <p>
+                                <span>*Mostre o QRCode para o frentista*</span>
+                            </p>
+
+                            <p>
+                                Ou se preferir, envie a chave do QRCode:
+                            </p>
+                            <p>
+                                <FontAwesomeIcon className="key-icon" icon={faKey} />: {qrCodeValue.chave}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
