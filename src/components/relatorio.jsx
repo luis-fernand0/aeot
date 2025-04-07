53afb4bb50a5466e7aae25b958b0e82c47b92dea
Luis Fernando <fernandoluis270@gmail.com>
2025-04-07 12:20:02 -0400

    adicionando um console.log() para verificar os dados enviados

--- src/components/relatorio.jsx
+++ src/components/relatorio.jsx
@@ -18,6 +18,7 @@ const Relatorio = () => {
   }
 
   async function filtrarDados() {
+    console.log(filtros)
     try {
       const response = await fetch('http://localhost:3000/aeot/auth/relatorio', {
         method: 'POST',
