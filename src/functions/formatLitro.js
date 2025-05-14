849c70c0ea23cf92b2e2b6bc2de77dea7e81fb99
luis-fernando-job <fernandoluis270@gmail.com>
2025-05-14 14:41:16 -0400

    formatando para 3 casas decimais

--- src/functions/formatLitro.js
+++ src/functions/formatLitro.js
@@ -5,17 +5,17 @@ export function formatLitro(e) {
         if (inputValue.length > 1) {
             inputValue = inputValue.slice(0, 1) + '.' + inputValue.slice(1);
         }
-        if (inputValue.length > 4) {
+        if (inputValue.length > 5) {
             inputValue = input.value.replace(/[^0-9]/g, '')
             inputValue = inputValue.slice(0, 2) + '.' + inputValue.slice(2);
         }
-        if (inputValue.length > 5) {
+        if (inputValue.length > 6) {
             inputValue = input.value.replace(/[^0-9]/g, '')
             inputValue = inputValue.slice(0, 3) + '.' + inputValue.slice(3);
         }
 
-        if (inputValue.length > 6) {
-            inputValue = inputValue.slice(0, 6)
+        if (inputValue.length > 7) {
+            inputValue = inputValue.slice(0, 7)
         }
 
         return input.value = inputValue
