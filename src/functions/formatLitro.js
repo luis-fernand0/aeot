9664eb22794f991a32213fbb015de89f35183ccf
Luis Fernando <fernandoluis270@gmail.com>
2025-05-17 12:37:51 -0400

    deletando os pontos de formatacoes e apenas limitando em 7 caracters

--- src/functions/formatLitro.js
+++ src/functions/formatLitro.js
@@ -1,18 +1,6 @@
 export function formatLitro(e) {
         let input = e.target
-        let inputValue = input.value.replace(/[^0-9]/g, '')
-
-        if (inputValue.length > 1) {
-            inputValue = inputValue.slice(0, 1) + '.' + inputValue.slice(1);
-        }
-        if (inputValue.length > 5) {
-            inputValue = input.value.replace(/[^0-9]/g, '')
-            inputValue = inputValue.slice(0, 2) + '.' + inputValue.slice(2);
-        }
-        if (inputValue.length > 6) {
-            inputValue = input.value.replace(/[^0-9]/g, '')
-            inputValue = inputValue.slice(0, 3) + '.' + inputValue.slice(3);
-        }
+        let inputValue = input.value.replace(/[^0-9,]/g, '')
 
         if (inputValue.length > 7) {
             inputValue = inputValue.slice(0, 7)
