99b522df5e125ff145bdd9e6b55d5fae7e68a8fc
luis-fernando-job <fernandoluis270@gmail.com>
2025-05-19 16:49:54 -0400

    limitando quantidade de virgula

--- src/functions/formatLitro.js
+++ src/functions/formatLitro.js
@@ -1,10 +1,35 @@
 export function formatLitro(e) {
-        let input = e.target
-        let inputValue = input.value.replace(/[^0-9,]/g, '')
+    let input = e.target
+    let inputValue = input.value.replace(/[^0-9,]/g, '')
 
-        if (inputValue.length > 7) {
-            inputValue = inputValue.slice(0, 7)
+    let caracter = ",";
+    let limite = 1;
+
+    function limitarVirgula(string, caractere, limite) {
+        let countVirgula = 0;
+        let novaString = "";
+
+        for (let i = 0; i < string.length; i++) {
+            if (string[i] === caractere) {
+                countVirgula++;
+                if (countVirgula > limite) {
+                    novaString += "";
+                } else {
+                    novaString += string[i];
+                }
+            } else {
+                novaString += string[i];
+            }
         }
 
-        return input.value = inputValue
+        return novaString;
     }
+
+    inputValue = limitarVirgula(inputValue, caracter, limite);
+
+    if (inputValue.length > 7) {
+        inputValue = inputValue.slice(0, 7)
+    }
+    
+    return input.value = inputValue
+}
