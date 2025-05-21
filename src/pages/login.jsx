6c5e813ff5754826b9f86f713149318a268c448c
luis-fernando-job <fernandoluis270@gmail.com>
2025-05-21 14:06:23 -0400

    versão 2.2.13 adicionando siglas FB, LL e ET no relatorio e excluido chave após 5 minutos

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -147,7 +147,7 @@ const Login = () => {
           </a>
 
           <br />
-          <p className='text-version'>2.2.12</p>
+          <p className='text-version'>2.2.13</p>
 
         </form >
 
