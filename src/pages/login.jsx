83d2b415db57c3a88d9f561a24a8140b92db5a13
luis-fernando-job <fernandoluis270@gmail.com>
2025-05-22 18:53:02 -0400

    versão 2.2.15 aumentando tempo do qrcode para 10 minutos e ordenando relatorio em ordem crescente

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -147,7 +147,7 @@ const Login = () => {
           </a>
 
           <br />
-          <p className='text-version'>2.2.14</p>
+          <p className='text-version'>2.2.15</p>
 
         </form >
 
