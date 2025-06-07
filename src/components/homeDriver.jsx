ba9d4a40a306fcd8f06b5d39819e0a2383f402eb
luis-fernando-job <fernandoluis270@gmail.com>
2025-06-07 09:22:15 -0400

    adapdatando o component para acessar um objeto

--- src/components/homeDriver.jsx
+++ src/components/homeDriver.jsx
@@ -3,8 +3,7 @@ import { faFlagCheckered, faClock } from '@fortawesome/free-solid-svg-icons'
 
 import '../style/homeDriver_component/homeDriver.css'
 
-const HomeDriver = ({ categoria, setCategoria, postos, detalhes, distancia, local }) => {
-    
+const HomeDriver = ({ categoria, setCategoria, postos, detalhes, distancia, local }) => {    
     return (
         <>
             <h2 className='title-home'>Todos os anuncios</h2>
@@ -21,53 +20,53 @@ const HomeDriver = ({ categoria, setCategoria, postos, detalhes, distancia, loca
                     </button>
                 </div>
                 <ul className='ul-gas-services'>
-                    {categoria.categoria === 'postos' && postos && postos.map((posto) =>
-                        <li onClick={() => { detalhes(posto, distancia[posto.cod_posto], local, categoria) }}
+                    {categoria.categoria === 'postos' && Object.keys(postos).map((posto) =>
+                        <li onClick={() => { detalhes(postos[posto], distancia[posto.cod_posto], local, categoria) }}
                             className='gas-services'
-                            key={posto.cod_posto}>
+                            key={postos[posto].cod_posto}>
 
                             <div className='container-img-title'>
                                 <div className='container-img'>
-                                    <img className='img-gas-services' src={`https://aeotnew.s3.amazonaws.com/${posto.foto}`} alt="imagem-do-posto-de-gasolina" />
+                                    <img className='img-gas-services' src={`https://aeotnew.s3.amazonaws.com/${postos[posto].foto}`} alt="imagem-do-posto-de-gasolina" />
                                 </div>
 
                                 <div className='container-title-endereco'>
                                     <h3 className='title-gas-services'>
-                                        {posto.nome}
+                                        {postos[posto].nome}
                                     </h3>
 
                                     <p className='endereco-gas-services'>
-                                        {posto.endereco}
+                                        {postos[posto].endereco}
                                     </p>
                                 </div>
                             </div>
                             <div className='info-gas-services'>
-                                {posto.combustivel?.etanol && (
+                                {postos[posto].combustiveis?.etanol && (
                                     <p className='combustiveis-gas-station'>
-                                        ETANOL: R$ {posto.combustivel?.etanol.valor}
+                                        ETANOL: R$ {postos[posto].combustiveis?.etanol.melhor_opcao.valor}
                                     </p>
                                 )}
 
-                                {posto.combustivel?.gasolina && (
+                                {postos[posto].combustiveis?.gasolina && (
                                     <p className='combustiveis-gas-station'>
-                                        GASOLINA: R$ {posto.combustivel?.gasolina.valor}
+                                        GASOLINA: R$ {postos[posto].combustiveis?.gasolina.melhor_opcao.valor}
                                     </p>
                                 )}
 
-                                {posto.combustivel?.diesel && (
+                                {postos[posto].combustiveis?.diesel && (
                                     <p className='combustiveis-gas-station'>
-                                        DIESEL: R$ {posto.combustivel?.diesel.valor}
+                                        DIESEL: R$ {postos[posto].combustiveis?.diesel.melhor_opcao.valor}
                                     </p>
                                 )}
 
-                                {distancia[posto.cod_posto] && (
+                                {distancia[postos[posto].cod_posto] && (
                                     <div className='container-km-time'>
                                         <p className='km km-time'>
-                                            <FontAwesomeIcon icon={faFlagCheckered} style={{ color: "#4caf50", }} /> {distancia[posto.cod_posto].distancia}
+                                            <FontAwesomeIcon icon={faFlagCheckered} style={{ color: "#4caf50", }} /> {distancia[postos[posto].cod_posto].distancia}
                                         </p>
 
                                         <p className='time km-time'>
-                                            <FontAwesomeIcon icon={faClock} style={{ color: "#4caf50", }} /> {distancia[posto.cod_posto].tempo}
+                                            <FontAwesomeIcon icon={faClock} style={{ color: "#4caf50", }} /> {distancia[postos[posto].cod_posto].tempo}
                                         </p>
                                     </div>
                                 )}
